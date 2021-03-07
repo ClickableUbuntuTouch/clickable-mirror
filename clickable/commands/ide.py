3df43c37252d1f6a18ce4414e86e3c485459ab70
Danfro <one@frostinfo.de>
2021-03-07 22:55:56 +0100

    add atom ide support

--- clickable/commands/ide.py
+++ clickable/commands/ide.py
@@ -2,6 +2,7 @@ from .desktop import DesktopCommand
 from clickable.logger import logger
 from clickable.exceptions import ClickableException
 from .idedelegates.qtcreator import QtCreatorDelegate
+from .idedelegates.atom import AtomDelegate
 
 class IdeCommand(DesktopCommand):
     aliases = []
@@ -15,12 +16,16 @@ class IdeCommand(DesktopCommand):
     def run(self, path_arg=None):
         if not path_arg:
             raise ClickableException('No command supplied for `clickable ide`')
-
         #get the preprocessor according to command if any
         if 'qtcreator' in path_arg.split():
             self.ide_delegate = QtCreatorDelegate()
             path_arg = self.ide_delegate.override_command(path_arg)
             logger.debug('QtCreator command detected. Changing command to: {}'.format(path_arg))
+
+        if 'atom' in path_arg.split():
+            self.ide_delegate = AtomDelegate()
+            path_arg = self.ide_delegate.override_command(path_arg)
+            logger.debug('Atom command detected. Changing command to: {}'.format(path_arg))
 
         self.command = path_arg
         super().run()
