2e7cf37b186fdb911973d3613236ab5fe907a86c
Brian Douglass <bhdouglass@gmail.com>
2021-07-07 21:33:56 -0400

    Fixed setting up gpg keys in ci

---
+++ scripts/setup_gpg_keys.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -x
+set -e
+
+echo "-----BEGIN PGP PUBLIC KEY BLOCK-----" > pub.gpg
+echo $GPG_PUBLIC | tr " " "\n" >> pub.gpg
+echo "-----END PGP PUBLIC KEY BLOCK-----" >> pub.gpg
+gpg --import pub.gpg
+rm pub.gpg
+
+echo "-----BEGIN PGP PRIVATE KEY BLOCK-----" > pri.gpg
+echo $GPG_PRIVATE | tr " " "\n" >> pri.gpg
+echo "-----END PGP PRIVATE KEY BLOCK-----" >> pri.gpg
+
+gpg --allow-secret-key-import --import pri.gpg
+rm pri.gpg
