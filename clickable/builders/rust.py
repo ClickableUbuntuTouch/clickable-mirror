9dbb551d94ad39a1b0b307e368a33517952970d8
Jonatan Hatakeyama Zeidler <jonatan_zeidler@gmx.de>
2021-09-11 09:41:30 +0200

    Use cargo install instead of trying to find binaries and copying them

--- clickable/builders/rust.py
+++ clickable/builders/rust.py
@@ -1,5 +1,4 @@
 import os
-import glob
 import shutil
 
 from clickable.config.constants import Constants
@@ -26,24 +25,16 @@ class RustBuilder(Builder):
         return rust_arch_target_mapping[self.config.build_arch]
 
     def build(self):
+        cargo_command = 'cargo install --target {} --target-dir {} --root {} --path {}'.format(
+            self._cargo_target, self.config.build_dir, self.config.install_dir, self.config.src_dir)
+
+        if self.debug_build:
+            cargo_command = '{} {}'.format(cargo_command, "--debug")
         if os.path.isdir(self.config.install_dir):
             shutil.rmtree(self.config.install_dir)
 
-        # Build using cargo
-        cargo_command = 'cargo build {} --target {} --target-dir {}' \
-            .format('--release' if not self.debug_build else '',
-                    self._cargo_target, self.config.build_dir)
 
         if self.config.build_args:
             cargo_command = '{} {}'.format(cargo_command, ' '.join(self.config.build_args))
 
-        self.container.run_command(cargo_command)
-
-        # There could be more than one executable
-        executables = glob.glob(os.path.join(
-            self.config.build_dir,
-            self._cargo_target,
-            'debug' if self.debug_build else 'release',
-            "*"))
-        for filename in filter(lambda f: os.path.isfile(f), executables):
-            self.config.install_bin.append(filename)
+        self.container.run_command(cargo_command, use_build_dir=False, cwd=self.config.src_dir)
