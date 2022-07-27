5a1ccf445291306d34899cc466e1836961fa64c7
Jonatan Hatakeyama Zeidler <jonatan_zeidler@gmx.de>
2022-07-27 15:10:35 +0200

    Remove armhf based focal image as long as the image can not be built

--- clickable/config/constants.py
+++ clickable/config/constants.py
@@ -38,7 +38,6 @@ class Constants():
         "armhf": {
             ('16.04.4', 'armhf'): 'clickable/armhf-16.04-armhf:16.04.4-qt5.9',
             ('16.04.5', 'armhf'): 'clickable/armhf-16.04-armhf:16.04.5',
-            ('20.04', 'armhf'): 'clickable/armhf-20.04-armhf:20.04',
         },
         "arm64": {
             ('16.04.4', 'arm64'): 'clickable/arm64-16.04-arm64:16.04.4-qt5.9',
