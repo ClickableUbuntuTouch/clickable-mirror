8147c6c601468253f15c2f5ed1a6da5ab9d8b6f7
Jonatan Hatakeyama Zeidler <jonatan_zeidler@gmx.de>
2023-03-12 19:15:09 +0100

    Only set CMake build type and cmake prefix if not yet set or on debug build

--- clickable/builders/cmake.py
+++ clickable/builders/cmake.py
@@ -1,6 +1,12 @@
+import re
+
 from clickable.config.constants import Constants
+from clickable.logger import logger
 
 from .make import MakeBuilder
+
+BUILD_TYPE = 'CMAKE_BUILD_TYPE'
+INSTALL_PREFIX = 'CMAKE_INSTALL_PREFIX'
 
 
 class CMakeBuilder(MakeBuilder):
@@ -10,18 +16,38 @@ class CMakeBuilder(MakeBuilder):
         self.container.run_command(f'make DESTDIR={self.config.install_dir}/ install')
 
     def build(self):
-        command = 'cmake'
-
-        if self.config.build_args:
-            joined_build_args = ' '.join(self.config.build_args)
-            command = f'{command} {joined_build_args}'
+        command = ['cmake']
 
         if self.debug_build:
-            command = f'{command} -DCMAKE_BUILD_TYPE=Debug'
-        else:
-            command = f'{command} -DCMAKE_BUILD_TYPE=Release'
+            logger.info('Setting CMake build type to "Debug"')
 
-        self.container.run_command(
-            f'{command} {self.config.src_dir} -DCMAKE_INSTALL_PREFIX:PATH=/.')
+            self.config.build_args = remove_arg(self.config.build_args, BUILD_TYPE)
+            self.config.build_args.append(f'-D{BUILD_TYPE}=Debug')
+        elif not has_arg(self.config.build_args, BUILD_TYPE):
+            logger.debug('Defaulting CMake build type to "Release"')
+
+            self.config.build_args.append(f'-D{BUILD_TYPE}=Release')
+
+        if not has_arg(self.config.build_args, INSTALL_PREFIX):
+            self.config.build_args.append(f'-D{INSTALL_PREFIX}=/.')
+
+        command += self.config.build_args
+        command.append(self.config.src_dir)
+
+        self.container.run_command(" ".join(command))
 
         super().build()
+
+
+def has_arg(args, name):
+    pattern = get_arg_pattern(name)
+    return any(pattern.match(a) for a in args)
+
+
+def remove_arg(args, name):
+    pattern = get_arg_pattern(name)
+    return [a for a in args if not pattern.match(a)]
+
+
+def get_arg_pattern(name):
+    return re.compile(r'-D\s*' + name + r'(:\w+)?=')
